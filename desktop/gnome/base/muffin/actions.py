8029eaf9dffe4b679a6b08c9b089cfc881549f8f
namso-01 <namso-01@hotmail.it>
2013-08-31 15:05:30 +0300

    gnome-packages

---
+++ desktop/gnome/base/muffin/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--prefix=/usr \
+                          --sysconfdir=/etc \
+                          --libexecdir=/usr/lib/muffin \
+                          --localstatedir=/var \
+                          --disable-static \
+                          --enable-compile-warnings=no \
+                          --disable-static \
+                          --enable-shape \
+                          --enable-sm \
+                          --enable-startup-notification \
+                          --enable-xsync \
+                          --enable-verbose-mode \
+                          --enable-compile-warnings=maximum \
+                          --with-libcanberra")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "NEWS", "README")
