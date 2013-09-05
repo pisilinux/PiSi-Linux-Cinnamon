bbaf7ef6c3b807bc6aca10cf73e1d4fea5f8a8d2
namso-01 <namso-01@hotmail.it>
2013-09-05 10:46:00 +0300

    accountsservice and polkit-gnome

--- desktop/gnome/core/accountsservice/actions.py
+++ desktop/gnome/core/accountsservice/actions.py
@@ -7,13 +7,15 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
 def setup():
-    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
-                         --enable-introspection=yes")
+                         --libexecdir=/usr/lib/accountsservice")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
