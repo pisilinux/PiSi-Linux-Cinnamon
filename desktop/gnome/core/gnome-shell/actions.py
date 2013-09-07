2a49122c3d28a86b198dfc69423bce8b28aec338
marcin- <marcin.bojara@gmail.com>
2013-09-07 20:30:11 +0200

    gnome-shell: fix build

--- desktop/gnome/core/gnome-shell/actions.py
+++ desktop/gnome/core/gnome-shell/actions.py
@@ -4,23 +4,24 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
     shelltools.system("NOCONFIGURE=1 ./autogen.sh")
-    #shelltools.system("intltoolize --force --copy --automake")
-    #autotools.autoreconf("-vif")
-    autotools.configure("--disable-static --enable-compile-warnings=no --disable-compile-warnings")
-                       
-    #pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared  -Wl,-rpath /usr/lib/gnome-bluetooth/")
+    autotools.configure("--disable-static \
+                         --disable-schemas-compile \
+                         --disable-compile-warnings \
+                        ")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     pisitools.dodoc("AUTHORS", "COPYING*", "NEWS", "README")
