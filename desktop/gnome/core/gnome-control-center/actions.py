8cd779793a81f294fa2c2a2686dd4ed210d6b270
marcin- <marcin.bojara@gmail.com>
2013-09-08 13:41:15 +0200

    shelltolls.system => pisitools.dosed

--- desktop/gnome/core/gnome-control-center/actions.py
+++ desktop/gnome/core/gnome-control-center/actions.py
@@ -14,16 +14,15 @@ def setup():
     shelltools.system("intltoolize --force --copy --automake")
     autotools.configure("--disable-static")
 
-    shelltools.system("""
-    sed -i -e 's/ -shared / -Wl,-O1,--as-needed\0 /g' -e 's/ if test "$export_dynamic" = yes && test -n "$export_dynamic_flag_spec"; then/ func_append compile_command " -Wl,-O1,--as-needed"\n func_append finalize_command " -Wl,-O1,--as-needed"\n\0/' libtool
-    """)
+    pisitools.dosed("libtool", "( -shared )", " -Wl,-O1,--as-needed\\1")
+    pisitools.dosed("libtool", '(    if test "\$export_dynamic" = yes && test -n "\$export_dynamic_flag_spec"; then)', '      func_append compile_command " -Wl,-O1,--as-needed"\n      func_append finalize_command " -Wl,-O1,--as-needed"\n\\1')
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
+
     pisitools.domove("/usr/share/pkgconfig/*","/usr/lib/pkgconfig")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
